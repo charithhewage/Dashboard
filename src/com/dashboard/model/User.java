030f5142b50ec963a02fb8f5dd0731f5d8ec2e4a
Charith Hewage <charithhewage@gmail.com>
2015-07-18 02:34:41 +0530

    sourse structure modified

---
+++ src/com/dashboard/model/User.java
@@ -0,0 +1,22 @@
+package com.dashboard.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="User")
+public class User implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	//@Column(name = "empid")
+
+}
